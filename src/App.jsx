9f79bcdad15f6b836be138f44167a551e5de7628
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-03-29 13:49:37 +0100

    create some of the routes in the App.jsx and links to the specific pages inside Header.jsx, test them from the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
-import MainPage from "./pages/MainPage";
 import Footer from "./components/layout/Footer";
+import MainPage from "./pages/MainPage";
+import CreateQuestion from "./components/questions/CreateQuestion";
+import QuestionList from "./components/questions/QuestionList";
+import QuestionDetails from "./components/questions/QuestionDetails";
+import EditQuestion from "./components/questions/EditQuestion";
+import DeleteQuestion from "./components/questions/DeleteQuestion";
 
 function App() {
-  
-
   return (
     <>
-     <Header />
-     <MainPage /> 
-     <Footer />
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/questions" element={<QuestionList />} />
+        <Route path="/questions/create" element={<CreateQuestion />} />
+        <Route path="/questions/:id" element={<QuestionDetails />} />
+        <Route path="/questions/:id/edit" element={<EditQuestion />} />
+        <Route path="/questions/:id/delete" element={<DeleteQuestion />} />
+        
+      </Routes>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
     return (
         <header>
             <h1>Either Or</h1>
             <nav>
-                
+                <ul>
+                    <li><Link to="/">🏠 Home</Link></li>
+                    <li><Link to="/questions">📋 Questions</Link></li>
+                    <li><Link to="/questions/create">➕ Create</Link></li>
+                </ul> 
             </nav>
         </header>
     )
