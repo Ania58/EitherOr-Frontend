c94832b985da26abbbd85e2e1cd55ea7922632b5
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-04-02 15:43:14 +0200

    configure Tailwind dependencies and create a new reusable component Spinner.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App'
+import App from './App';
+import "./index.css";
 import UserProvider from './components/contexts/UserContext';
 
 createRoot(document.getElementById('root')).render(
