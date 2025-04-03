d3c239b374ea438a5dcc3de9e829009daf1dcf9e
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-04-03 23:34:18 +0200

    Improve Footer styling with Tailwind

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     const fullYear = new Date().getFullYear();
 
     return (
-        <footer>
+        <footer className="text-center text-sm text-gray-500 py-4 mt-8 border-t">
             <p>© {fullYear} Either Or. All Rights Reserved.</p>
         </footer>
     )
